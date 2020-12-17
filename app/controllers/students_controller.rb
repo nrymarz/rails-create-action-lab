9e5798fafc8b91d198904cfd5f73a86f1ce3199a
nrymarz <nrymarz@gmail.com>
2020-12-16 21:27:35 -0600

    commit tree 46583fbc13b9d5e4365283638959fc8cae437fd6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+
 class StudentsController < ApplicationController
   
   def index
@@ -12,6 +13,8 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(first_name: params[:first_name],last_name: params[:last_name])
+    redirect_to student_path(@student)
   end
 
 end
